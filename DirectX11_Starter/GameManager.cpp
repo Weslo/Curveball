d50209ccab5b6df8479384421c539a6f4b48e5c3
kmurn3 <krm1419@OH1375-32.main.ad.rit.edu>
2015-04-16 09:55:18 -0400

    Changed sampler
    Sampler now does Anisotropic instead of linear

--- DirectX11_Starter/GameManager.cpp
+++ DirectX11_Starter/GameManager.cpp
@@ -137,7 +137,9 @@ void GameManager::CreateSamplerState()
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	//samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDesc.MaxAnisotropy = 16;
 	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 	device->CreateSamplerState(&samplerDesc, &samplerState);
 
